1cfceb8321da6d380dea665983c4c826025dbdb4
TAV <tereshenkov@mail.ru>
2024-01-26 18:53:42 +0300

    Добавлен бот и его движения по команде игрока с учетом лабиринта.

--- src/core/scenetest.pas
+++ src/core/scenetest.pas
@@ -1,13 +1,15 @@
-unit SceneTest;
+﻿unit SceneTest;
 
 interface
 
 uses
   Classes, SysUtils,
   SfmlSystem,SfmlWindow,SfmlGraphics,
-  Helpers, Scene, Level ;
+  Helpers, Scene, Level, SfmlAnimation ;
 
 type
+
+  TCommand = ( cmdNone, cmdStop, cmdLeft, cmdRight, cmdUp, cmdDown ) ;
 
   { TSceneTest }
 
@@ -20,6 +22,22 @@ type
     spr_icons,spr_icons_gray:array of TSfmlSprite ;
     level:TLevel ;
     textLevel:TSfmlText ;
+    waitbot:TSfmlSprite ;
+    walkbot:TSfmlAnimation ;
+    // Позиция игрока
+    player_x:Single ;
+    player_y:Single ;
+    // Скорость игрока
+    player_dx:Integer ;
+    player_dy:Integer ;
+    tek_cmd:TCommand ;
+    ismirr:Boolean ;
+    left_scale_bot:TSfmlVector2f ;
+    right_scale_bot:TSfmlVector2f ;
+    function isWayCorrect(x, y: Integer): Boolean;
+    procedure setCmdToDxy();
+    function fixXifCrossed(stopx, dx:Single):Boolean ;
+    function fixYifCrossed(stopy, dy:Single):Boolean ;
   public
     function Init():Boolean ; override ;
     function FrameFunc(dt:Single; events:TUniList<TSfmlEventEx>):TSceneResult ; override ;
@@ -33,7 +51,115 @@ uses SfmlUtils, CommonData ;
 const CELL_HEIGHT=40 ;
       CELL_WIDTH=40 ;
 
+      PLAYER_SPEED = 5 ;
+
+
+function cmd2sig(cmd:TCommand):Integer ;
+begin
+  case cmd of
+    cmdNone: Result:=0;
+    cmdStop: Result:=0;
+    cmdLeft: Result:=-1;
+    cmdRight: Result:=1;
+    cmdUp: Result:=-1;
+    cmdDown: Result:=1;
+  end;
+end;
+
+function isDXDYRevers(dx1, dy1, dx2, dy2: Integer): Boolean;
+begin
+  if (dy1=0)and(dy2=0) then Result:=dx1=-dx2 else
+  if (dx1=0)and(dx2=0) then Result:=dy1=-dy2 else
+  Result:=False ;
+end;
+
 { TSceneTest }
+
+function TSceneTest.isWayCorrect(x, y: Integer): Boolean;
+begin
+  Result:=False ;
+  if x<0 then Exit ;
+  if y<0 then Exit ;
+  if x>=level.getWidth() then Exit ;
+  if y>=level.getHeight() then Exit ;
+  if level.isBlockAt(x,y) then Exit ;
+  Result:=True ;
+end;
+
+procedure TSceneTest.setCmdToDxy();
+begin
+  if tek_cmd<>cmdNone then begin
+    case tek_cmd of
+      cmdStop: begin
+        player_dx:=0 ;
+        player_dy:=0 ;
+      end;
+      cmdLeft: begin
+        player_dx:=-1 ;
+        player_dy:=0 ;
+      end;
+      cmdRight: begin
+        player_dx:=1 ;
+        player_dy:=0 ;
+      end;
+      cmdUp: begin
+        player_dx:=0 ;
+        player_dy:=-1 ;
+      end;
+      cmdDown: begin
+        player_dx:=0 ;
+        player_dy:=1 ;
+      end;
+    end;
+    tek_cmd:=cmdNone ;
+  end;
+end;
+
+function TSceneTest.fixXifCrossed(stopx, dx:Single):Boolean ;
+var borderx:Single ;
+begin
+    if (player_dx=-1) then begin
+        borderx := player_x-0 ;
+        if (borderx+dx<stopx) then begin
+            player_x := stopx;
+            Result:=True ;
+        end
+        else
+          Result:=False ;
+    end
+    else begin
+        borderx := player_x+1 ;
+        if (borderx+dx>stopx) then begin
+            player_x := stopx-0 ;
+            Result:=True ;
+        end
+        else
+          Result:=False ;
+    end ;
+end ;
+
+function TSceneTest.fixYifCrossed(stopy, dy:Single):Boolean ;
+var bordery:Single ;
+begin
+    if (player_dy=-1) then begin
+        bordery := player_y-0 ;
+        if (bordery+dy<stopy) then begin
+            player_y := stopy ;
+            Result:=True ;
+        end
+        else
+          Result:=False ;
+    end
+    else begin
+        bordery := player_y+1 ;
+        if (bordery+dy>stopy) then begin
+            player_y := stopy-0 ;
+            Result:=True ;
+        end
+        else
+          Result:=False ;
+    end ;
+end ;
 
 function TSceneTest.Init():Boolean ;
 var i:Integer ;
@@ -70,8 +196,27 @@ begin
 
   textLevel:=createText(TCommonData.Font,'LEVEL 1',18,SfmlWhite) ;
 
+  waitbot:=loadSprite('images'+PATH_SEP+'waitbot.png');
+  waitbot.Origin:=SfmlVector2f(SfmlTextureGetSize(waitbot.Texture).x/2,29) ;
+  waitbot.Scale(0.75,0.75) ;
+  walkbot:=TSfmlAnimation.Create('images'+PATH_SEP+'walkbot.png',5,8);
+  walkbot.Origin:=waitbot.Origin ;
+  walkbot.Scale(0.75,0.75) ;
+  walkbot.Play() ;
+
   level:=TLevel.Create ;
   level.LoadFromFile('levels'+PATH_SEP+'level1.dat');
+
+  tek_cmd:=cmdNone ;
+  player_dx:=0 ;
+  player_dy:=0 ;
+  player_x:=1 ;
+  player_y:=1 ;
+  ismirr:=False ;
+
+  left_scale_bot:=SfmlVector2f(-0.75,0.75) ;
+  right_scale_bot:=SfmlVector2f(0.75,0.75) ;
+
   Result:=True ;
 end ;
 
@@ -81,8 +226,62 @@ begin
   Result:=Normal ;
   for event in events do
     if (event.event.EventType = sfEvtKeyPressed) then begin
-      if (event.event.key.code = sfKeyEscape) then Exit(TSceneResult.Close)
+      if (event.event.key.code = sfKeyEscape) then Exit(TSceneResult.Close) ;
+      if (event.event.key.code = sfKeyLeft) then tek_cmd:=cmdLeft ;
+      if (event.event.key.code = sfKeyRight) then tek_cmd:=cmdRight ;
+      if (event.event.key.code = sfKeyUp) then tek_cmd:=cmdUp ;
+      if (event.event.key.code = sfKeyDown) then tek_cmd:=cmdDown ;
+      if (event.event.key.code = sfKeySpace) then tek_cmd:=cmdStop ;
     end ;
+
+  // Движение игрока
+  // Смена направления
+  if (tek_cmd=cmdDown)or(tek_cmd=cmdUp) then
+        if isWayCorrect(Trunc(player_x),Trunc(player_y)+cmd2sig(tek_cmd)) then begin
+            if player_dx=1 then begin
+                if fixXifCrossed(Trunc(player_x),player_dx*PLAYER_SPEED*dt) then setCmdToDxy() ;
+            end
+            else begin
+                if (fixXifCrossed(Trunc(player_x),player_dx*PLAYER_SPEED*dt)) then setCmdToDxy() ;
+            end ;
+        end ;
+
+  if (tek_cmd=cmdLeft)or(tek_cmd=cmdRight) then
+        if isWayCorrect(Trunc(player_x)+cmd2sig(tek_cmd),Trunc(player_y)) then begin
+            if player_dy=1 then begin
+                if fixYifCrossed(Trunc(player_y),player_dy*PLAYER_SPEED*dt) then setCmdToDxy() ;
+            end
+            else begin
+                if (fixYifCrossed(Trunc(player_y),player_dy*PLAYER_SPEED*dt)) then setCmdToDxy() ;
+            end ;
+        end ;
+
+  if tek_cmd=cmdStop then begin
+    player_dx:=0 ;
+    player_dy:=0 ;
+  end ;
+
+  // Торможение о стены
+  if player_dx=-1 then
+    if not isWayCorrect(Trunc(player_x)-1,Trunc(player_y)) then
+      if fixXifCrossed(Trunc(player_x),player_dx*PLAYER_SPEED*dt) then player_dx:=0 ;
+  if player_dx=1 then
+    if not isWayCorrect(Trunc(player_x)+1,Trunc(player_y)) then
+      if fixXifCrossed(Trunc(player_x),player_dx*PLAYER_SPEED*dt) then player_dx:=0 ;
+  if player_dy=-1 then
+    if not isWayCorrect(Trunc(player_x),Trunc(player_y)-1) then
+      if fixYifCrossed(Trunc(player_y),player_dy*PLAYER_SPEED*dt) then player_dy:=0 ;
+  if player_dy=1 then
+    if not isWayCorrect(Trunc(player_x),Trunc(player_y)+1) then
+      if fixYifCrossed(Trunc(player_y),player_dy*PLAYER_SPEED*dt) then player_dy:=0 ;
+
+  player_x:=player_x+player_dx*PLAYER_SPEED*dt ;
+  player_y:=player_y+player_dy*PLAYER_SPEED*dt ;
+
+  if player_dx=-1 then ismirr:=True ;
+  if player_dx=1 then ismirr:=False ;
+
+  walkbot.Update(dt) ;
 end ;
 
 procedure TSceneTest.RenderFunc() ;
@@ -108,6 +307,20 @@ begin
 
   DrawTextCentered(textLevel,(CELL_WIDTH*23+wwidth)/2,10) ;
 
+  if ismirr then begin
+    waitbot.ScaleFactor:=left_scale_bot ;
+    walkbot.ScaleFactor:=left_scale_bot ;
+  end
+  else begin
+    waitbot.ScaleFactor:=right_scale_bot ;
+    walkbot.ScaleFactor:=right_scale_bot ;
+  end;
+
+  if (player_dx=0)and(player_dy=0) then
+    DrawSprite(waitbot, CELL_WIDTH*player_x + 20, CELL_HEIGHT*player_y)
+  else
+    DrawSprite(walkbot, CELL_WIDTH*player_x + 20, CELL_HEIGHT*player_y) ;
+
   DrawSprite(cursor,mx,my) ;
 end ;
 
@@ -117,6 +330,8 @@ begin
   spr_block.Free ;
   spr_stair.Free ;
   spr_crystall.Free ;
+  walkbot.Free ;
+  waitbot.Free ;
   textLevel.Free ;
 end ;
 
