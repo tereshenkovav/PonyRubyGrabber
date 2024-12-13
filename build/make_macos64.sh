e4788bd073c3b49c79857c7d71a552f1a7fa6b03
TAV <tereshenkov@mail.ru>
2024-12-13 11:41:37 +0300

    Исправления в скрипте сборки MacOS.

--- build/make_macos64.sh
+++ build/make_macos64.sh
@@ -1,2 +1,3 @@
 mkdir x86_64-darwin
+mkdir ../bin
 ppcx64 @fpc-config -Fl/usr/local/lib -k"-lcsfml-graphics.2.5.1 -lcsfml-system.2.5.1 -lcsfml-window.2.5.1 -lcsfml-audio.2.5.1 -rpath @executable_path/../Frameworks" ../src/PonyRubyGrabberFPC.pp
